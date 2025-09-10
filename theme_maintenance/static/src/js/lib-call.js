ec174c4ed2f273ae5ae84b29931b72aeed184544
Hasnaa <hasnaaayman36@gmail.com>
2025-09-10 17:02:10 +0300

    update testimonial carousel settings

--- theme_maintenance/static/src/js/lib-call.js
+++ theme_maintenance/static/src/js/lib-call.js
@@ -27,5 +27,33 @@ $(document).ready(function () {
         }
     });
 });
+$(".carousel-about").owlCarousel({
+    loop: true,
+    center: true,
+    items: 1,
+    stagePadding: 200,
+    margin: 32,
+    autoplay: true,
+    autoplayTimeout: 20000,
+    responsive: {
+        0: {stagePadding: 0, items: 1},
+        768: {stagePadding: 100, items: 1.9},
+        1200: {stagePadding: 200, items: 1.9}
+    }
+});
+$(".team").owlCarousel({
+    loop: true,
+    margin: 39,
+    nav: true,
+    navText: ["<i class='bi bi-arrow-left'></i>", "<i class='bi bi-arrow-right'></i>"],
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 20000,
+    responsive: {
+        0: {items: 1},
+        768: {items: 2},
+        1200: {items: 4}
+    }
+});
 
 
