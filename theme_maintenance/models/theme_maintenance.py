e5d191a4e887904076754d462a3b85e7980eb6a7
YoussefHussein57 <118389891+YoussefHussein57@users.noreply.github.com>
2025-09-16 15:16:44 +0300

    updates

--- theme_maintenance/models/theme_maintenance.py
+++ theme_maintenance/models/theme_maintenance.py
@@ -5,5 +5,8 @@ class ThemeMaintenance(models.AbstractModel):
     _inherit = 'theme.utils'
 
     def _theme_maintenance_post_copy(self, mod):
+        self.disable_view('website_blog.opt_blog_post_breadcrumb')
+        self.enable_view('website_blog.opt_blog_post_regular_cover')
+        self.disable_view('website_blog.opt_blog_post_readable')
         self.disable_view('website_blog.opt_blog_cover_post_fullwidth_design')
         self.disable_view('website.footer_custom')
